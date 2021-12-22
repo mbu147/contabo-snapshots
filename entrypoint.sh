8bb805479df652c62a0e697de6608240c95bf01f
Martin Baumert <marb@xxxlgroup.com>
2021-12-22 12:02:35 +0100

    add exit codes (for healthchecks)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 # authenticate
 cntb config set-credentials --oauth2-clientid="${CLIENTID}" --oauth2-client-secret="${CLIENTSECRET}" --oauth2-user="${USER}" --oauth2-password="${PASSWORD}"
 
-# get instance id's
-INSTANCES=$(cntb get instances -o json |jq -r '.[].instanceId')
+# check connection
+if ! cntb get instances; then exit 1; fi
 
-for INSTANCE in ${INSTANCES};
+# for instance ids
+for INSTANCE in $(cntb get instances -o json |jq -r '.[].instanceId');
 do
     # check if old snapshot exist
     if [ "$(cntb get snapshots "${INSTANCE}" -o json --name "daily" |jq -r '.[].snapshotId')" ]; then
@@ -17,6 +18,5 @@ do
     fi
     # create new snapshot
     echo "creating snapshot for ${INSTANCE}"
-    cntb create snapshot "${INSTANCE}" --name daily
+    if ! cntb create snapshot "${INSTANCE}" --name daily; then exit 1; fi
 done
-
